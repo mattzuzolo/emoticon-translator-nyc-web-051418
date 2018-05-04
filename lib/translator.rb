8cc85b837e37d315bc8eab9d56e16acb9a972299
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-04 00:33:37 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,58 @@ require 'pry'
 def load_library (file_path)
   
   emoticon_file = YAML.load_file(file_path)
+  
+
+  emoticon_dictionary = {}
+  emoticon_dictionary["get_meaning"] = {}
+  emoticon_dictionary["get_emoticon"] = {}
+  
+  
+  emoticon_file.each do |emoticon_text, emoticon_icons_array|
+    
+    emoticon_icon_array.each_with_index { |item, index|
+      if index == 0
+          usa = item
+      elsif index == 1
+          jp = item
+      end
+    }
+    
+      emoticon_dictionary["get_meaning"][usa] = {emoticon_icons_array}
+      emoticon_dictionary["get_meaning"][jp] = {emoticon_text}
+    
+    emoticon_dictionary
+  end
+  
+  
+    #binding.pry
+  emoticon_file.each do |emoticon_text, emoticon_icons_array|
+    #     emoticon_icons_array.each_with_index { |item, index|
+    #     #binding.pry
+        if index == 0
+          emoticon_dictionary["get_emoticon"] << emoticon_icons_array[index]
+        elsif index == 1
+          emoticon_dictionary["get_meaning"] << emoticon_icons_array[index]
+        end
+        
+    # }
+      
+       #binding.pry
+    
+    
+    
+  end #end file each
+  
+  emoticon_dictionary
  # binding.pry
-end
+end #end load_library
+
+
+
+
+# emoticon_text_array << emoticon_text
+
+
 
 def get_japanese_emoticon
   # code goes here
@@ -15,3 +65,27 @@ end
 def get_english_meaning
   # code goes here
 end
+
+
+
+
+
+
+
+
+
+
+
+  
+  # emoticon_file.each do |emoticon_text, emoticon_icon_array|
+  #   emoticon_text_array << emoticon_text
+    
+    # emoticon_icon_array.each_with_index { |item, index|
+    
+    #     if index == 0
+    #       emoticon_us << emoticon_icon_array[index]
+    #     elsif index == 1
+    #       emoticon_jp << emoticon_icon_array[index]
+    #     end
+    # }
+    
